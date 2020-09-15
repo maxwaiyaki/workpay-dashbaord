1bbe0f80ece9d714af8eb5e516bc638ff36b6bfa
Waiyaki <maxwel.wairimu@gmail.com>
2020-09-15 07:47:49 +0300

    Comprehensive project setup: Linting, Redux, Material UI etc..

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import FontFaceObserver from 'fontfaceobserver';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const robotoObserver300 = new FontFaceObserver('Roboto', { weight: 300 });
+const robotoObserver400 = new FontFaceObserver('Roboto', { weight: 400 });
+const robotoObserver500 = new FontFaceObserver('Roboto', { weight: 500 });
+const robotoObserver700 = new FontFaceObserver('Roboto', { weight: 700 });
+
+// When Roboto is loaded, add a font-family using Roboto to the body
+Promise.all([
+	robotoObserver300.load(),
+	robotoObserver400.load(),
+	robotoObserver500.load(),
+	robotoObserver700.load(),
+]).then(() => {
+	document.body.classList.add('fontLoaded');
+});
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
