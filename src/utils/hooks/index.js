6767c45b5ecaec03950a24601f1d00acc818a462
Waiyaki <maxwel.wairimu@gmail.com>
2020-09-24 18:17:58 +0300

    add useDiclosure hook

--- src/utils/hooks/index.js
+++ src/utils/hooks/index.js
@@ -1,3 +1,17 @@
+import { useState, useCallback } from 'react';
+
+// custom hook to help handle common open, close, or toggle scenarios
+export function useDisclosure(defaultIsOpen) {
+	const [isOpen, setIsOpen] = useState(Boolean(defaultIsOpen));
+	const onClose = useCallback(() => setIsOpen(false), []);
+	const onOpen = useCallback(() => setIsOpen(true), []);
+	const onToggle = useCallback(
+		() => setIsOpen((prevIsOpen) => !prevIsOpen),
+		[],
+	);
+	return { isOpen, onOpen, onClose, onToggle };
+}
+
 export function useDateTime() {
 	const today = new Date();
 	const curHr = today.getHours();
